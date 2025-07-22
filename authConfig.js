39e92ccfcf5e494412b021f8958c69577073b969
itc studio <itcstudio@itcs-iMac.local>
2025-07-22 13:02:28 +0700

    add trust host

--- authConfig.js
+++ authConfig.js
@@ -2,7 +2,11 @@ export const authConfig = {
   pages: {
     signIn: '/login', // Custom login page
   },
-   trustHost: true,
+  trustHost: true,
+  session: {
+    strategy: "jwt", // or "database" if you're using a DB
+  },
+  secret: process.env.AUTH_SECRET,
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
      const isLoggedIn = !!auth?.user;
