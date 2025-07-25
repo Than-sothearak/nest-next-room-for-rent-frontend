ea1510e053c3243d4a2eabdfffaf923ec6de9b59
Than-sothearak <thearak643@gmail.com>
2025-07-25 15:29:43 +0700

    fixed booking not define

--- actions/rooms.js
+++ actions/rooms.js
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import { deleteFileFromS3, uploadFileToS3 } from "@/utils/uploadImageFileToS3";
 import { Room } from "@/models/Room";
 import { auth } from "@/auth";
+import { Booking } from "@/models/Booking";
 
 export async function getRoom(query, page, sort) {
   await mongoDb();
