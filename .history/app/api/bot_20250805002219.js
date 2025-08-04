02f9ca778e8afb6c0d4bc9ac3f4a18d1a806ca31
Than-sothearak <thearak643@gmail.com>
2025-08-05 00:22:36 +0700

    test4

---
+++ .history/app/api/bot_20250805002219.js
@@ -0,0 +1,10 @@
+import { Bot, webhookCallback } from "grammy";
+import "dotenv/config";
+
+const token = process.env.TELEGRAM_BOT_TOKEN
+if (!token) throw new Error("BOT_TOKEN is unset");
+const bot = new Bot(token); // ✅ Now using env variable
+export const config = {
+  runtime: "edge",
+};
+export default webhookCallback(bot, "https");
