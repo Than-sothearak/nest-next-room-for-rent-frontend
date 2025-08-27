89cd99563d725bea6e0b50cb31d9052c8954d776
than-sothearak <thearak643@gmail.com>
2025-08-27 10:06:40 +0700

    fixed auth user id to string

--- auth.js
+++ auth.js
@@ -48,7 +48,7 @@ export const { auth, signIn, signOut } = NextAuth({
         token.imageUrl = user.imageUrl;
         token.email = user.email;
         token.isAdmin = user.isAdmin;
-        token._id = user._id;
+        token._id = user._id.toString();
       }
 
       return token;
@@ -56,7 +56,7 @@ export const { auth, signIn, signOut } = NextAuth({
     async session({ session, token }) {
       if (token) {
         session.user = {
-          _id: token._id,
+          _id: token._id.toString(),
           username: token.username,
           email: token.email,
           imageUrl: token.imageUrl,
